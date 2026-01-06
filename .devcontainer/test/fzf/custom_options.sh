a070c536af597d52d9cbc01eec03e2be80dfea2e
Joshua Ward <j2udev@gmail.com>
2026-01-06 16:40:12 +0000

    feat: added fzf feature to add better history and file finding

---
+++ .devcontainer/test/fzf/custom_options.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+# Import test library bundled with the devcontainer CLI
+source dev-container-features-test-lib
+
+# Test that fzf was installed
+check "fzf command is available" fzf --version
+
+# Test that shell integration file exists
+check "fzf-feature.sh exists in shellrc.d" test -f "$HOME/.shellrc.d/fzf-feature.sh"
+
+# Test that custom FZF_DEFAULT_OPTS are set
+check "Custom preview option is configured" grep -q "preview 'cat {}'" "$HOME/.shellrc.d/fzf-feature.sh"
+
+# Test that height 50% is set (custom value, not default 40%)
+check "Custom height option is configured" grep -q "height 50%" "$HOME/.shellrc.d/fzf-feature.sh"
+
+# Report results
+reportResults
