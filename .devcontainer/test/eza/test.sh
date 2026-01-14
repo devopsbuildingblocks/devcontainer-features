04b32d7c279f523c22747a48bba249a9fd231142
Joshua Ward <j2udev@gmail.com>
2026-01-14 16:37:06 +0000

    chore: attempting to fix tests that need to set up devbox global environment

--- .devcontainer/test/eza/test.sh
+++ .devcontainer/test/eza/test.sh
@@ -5,6 +5,11 @@ set -e
 # Import test library bundled with the devcontainer CLI
 source dev-container-features-test-lib
 
+# Source devbox shell integration to get devbox global packages in PATH
+if [ -f "$HOME/.shellrc.d/devbox-feature.sh" ]; then
+    source "$HOME/.shellrc.d/devbox-feature.sh"
+fi
+
 # Test 1: eza command is available
 check "eza command is available" eza --version
 
