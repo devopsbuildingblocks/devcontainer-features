596d5e7738c29a76218ced6ae3aad3449d793a05
Joshua Ward <j2udev@gmail.com>
2026-01-26 20:18:08 +0000

    chore: updating tests

--- .devcontainer/test/common-utils/test.sh
+++ .devcontainer/test/common-utils/test.sh
@@ -11,19 +11,16 @@ check "shellrc.d directory exists" test -d "$HOME/.shellrc.d"
 # Test 2: main.sh loader exists
 check "main.sh loader exists" test -f "$HOME/.shellrc.d/main.sh"
 
-# Test 3: common-utils feature config exists
-check "common-utils-feature.sh exists" test -f "$HOME/.shellrc.d/common-utils-feature.sh"
-
-# Test 4: zsh is available
+# Test 3: zsh is available
 check "zsh is installed" command -v zsh
 
-# Test 5: .bashrc sources shellrc.d/main.sh
+# Test 4: .bashrc sources shellrc.d/main.sh
 check ".bashrc sources shellrc.d/main.sh" grep -q "shellrc.d/main.sh" "$HOME/.bashrc"
 
-# Test 6: .zshrc sources shellrc.d/main.sh
+# Test 5: .zshrc sources shellrc.d/main.sh
 check ".zshrc sources shellrc.d/main.sh" grep -q "shellrc.d/main.sh" "$HOME/.zshrc"
 
-# Test 7: postCreateCommand script exists
+# Test 6: postCreateCommand script exists
 check "common-utils-postCreateCommand.sh exists and is executable" test -x "/usr/local/bin/common-utils-postCreateCommand.sh"
 
 # Report results
